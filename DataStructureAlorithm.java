bc56dd0f5af3d89815ee9c618cdf5ed5b41fb354
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-09 18:03:46 +0530

    Added:Program for the Data Structure and Algorithm

---
+++ DataStructureAlorithm.java
@@ -0,0 +1,9 @@
+package com.bz.datastructucturealorithm;
+
+public class DataStructureAlorithm {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Data Structure Alorithm");
+	}
+
+}
